6357ddc3e1a95e478b0b50a06d133df33b237f7d
yuriy <fivestarter@gmail.com>
2015-01-16 22:55:37 +0300

    add buttons

--- game/src/main/java/ru/fivestarter/android/game/MainActivity.java
+++ game/src/main/java/ru/fivestarter/android/game/MainActivity.java
@@ -4,14 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+    private TextView textView;
+    private Button noButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        noButton = (Button) findViewById(R.id.button2);
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hello world!");
+            }
+        });
+    }
+
+
+    public void sayHello(View view) {
+        textView.setText("! @ #");
     }
 
 
