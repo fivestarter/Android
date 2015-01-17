3933fee35b1644491ee191584881c0e1c200ecba
yuriy <fivestarter@gmail.com>
2015-01-17 20:36:57 +0300

    11. alert dialog

--- game/src/main/java/ru/fivestarter/android/game/activity/MainActivity.java
+++ game/src/main/java/ru/fivestarter/android/game/activity/MainActivity.java
@@ -1,81 +1,54 @@
 package ru.fivestarter.android.game.activity;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 import ru.fivestarter.android.game.R;
-import ru.fivestarter.android.game.util.RequestCode;
 
 /**
  * @author yuriy on 17.01.15.
  */
 public class MainActivity extends Activity {
 
-    private TextView textName;
-    private TextView textLanguage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        textName = (TextView) findViewById(R.id.textName);
-        textLanguage = (TextView) findViewById(R.id.textLanguage);
     }
 
-/*    public void onShow(View view) {
-        Intent intent;
-
-        switch (view.getId()) {
-            case R.id.btnPresented:
-                intent = new Intent(this, PresentedActivity.class);
-                startActivityForResult(intent, RequestCode.PRESENTED.getCode());
-                break;
-
-            case R.id.btnLanguage:
-                intent = new Intent(this, LanguageActivity.class);
-                startActivityForResult(intent, RequestCode.LANGUAGE.getCode());
-                break;
-
-            default:
-                break;
-        }
-    }*/
-
-    public void onPresent(View view) {
-        Intent intent = new Intent(this, PresentedActivity.class);
-        startActivityForResult(intent, RequestCode.PRESENTED);
-
-    }
-
-    public void onChoiceLanguage(View view) {
-        Intent intent = new Intent(this, LanguageActivity.class);
-        startActivityForResult(intent, RequestCode.LANGUAGE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case RequestCode.PRESENTED:
-                    String name = data.getStringExtra("name");
-                    textName.setText("Рад знакомству " + name);
-                    break;
-
-                case RequestCode.LANGUAGE:
-                    String language = data.getStringExtra("language");
-                    textLanguage.setText("Ваш язык: " + language);
-                    break;
-
-                default:
-                    break;
+    public void showDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Title");
+        builder.setMessage("It is dialog");
+        builder.setIcon(R.drawable.ic_launcher);
+        builder.setCancelable(true);
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast toast = Toast.makeText(MainActivity.this, "No", Toast.LENGTH_SHORT);
+                toast.show();
             }
+        });
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast toast = Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                Toast toast = Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
-        } else {
-            Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
-        }
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
