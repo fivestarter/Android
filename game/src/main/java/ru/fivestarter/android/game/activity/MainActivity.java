67390e68996d309de9df1aea55df4a9ebc0f4192
yuriy <fivestarter@gmail.com>
2015-01-30 08:32:09 +0300

    21. notification

--- game/src/main/java/ru/fivestarter/android/game/activity/MainActivity.java
+++ game/src/main/java/ru/fivestarter/android/game/activity/MainActivity.java
@@ -1,19 +1,15 @@
 package ru.fivestarter.android.game.activity;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 import ru.fivestarter.android.game.R;
 
 /**
@@ -49,8 +45,19 @@ public class MainActivity extends Activity {
         builder.setAutoCancel(true);
         builder.setContentTitle("Уведомление");
         builder.setContentText("Press if you want know a secret");
+        builder.setProgress(100, 20, false);
 
         Notification build = builder.build();
+       build.defaults = Notification.DEFAULT_SOUND;
+//        build.sound = Uri.parse("android.resource://ru.fivestarter.android.game/" + R.raw.blind);*/
+//        long[] vibrate = new long[] {1500, 1000, 1500, 1000};
+//        build.vibrate = vibrate;
+
+        build.flags = build.flags | Notification.FLAG_INSISTENT;
         nm.notify(NOTIFICATION_ID, build);
+    }
+
+    public void cancelNotification(View view) {
+        nm.cancel(NOTIFICATION_ID);
     }
 }
